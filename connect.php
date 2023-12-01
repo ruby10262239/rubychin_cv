6014bdad9e4d5f5da49d959206bb5cb8bdd7651c
ruby10262239 <55231689+ruby10262239@users.noreply.github.com>
2023-12-01 11:58:31 +0800

    php connect

--- connect.php
+++ connect.php
@@ -3,7 +3,19 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $comments = $_POST['comments'];
-    $stars = $_POST['comments'];
+    $stars = $_POST['stars'];
 
-    //Data connection
+    // Data connection
+    $conn =new mysqli('localhost', 'root','','feedback');
+    if($conn->connect_error){
+        die('Connection failed : ' $conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into feedback(name, email, comments, stars)
+        values(?,?,?,?)")
+        $stmt->bind_param("sssi", $name, $email, $comments, $stars);
+        $stmt->execute();
+        echo "Submitted Successfully"
+        $stmt->close();
+        $conn->close();
+    }
 ?>
