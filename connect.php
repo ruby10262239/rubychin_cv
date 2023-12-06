6d18b69cda227bddf6a7f3ff075b948952b14e5a
ruby10262239 <55231689+ruby10262239@users.noreply.github.com>
2023-12-06 12:40:57 +0800

    modify codes

--- connect.php
+++ connect.php
@@ -1,21 +1,21 @@
 <?php
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $comments = $_POST['comments'];
-    $stars = $_POST['stars'];
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $comments = $_POST['comments'];
+    // $stars = $_POST['stars'];
 
-    // Data connection
-    $conn =new mysqli('localhost', 'root','','feedback');
-    if($conn->connect_error){
-        die('Connection failed : ' .$conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into feedback(name, email, comments, stars)
-        values(?,?,?,?)");
-        $stmt->bind_param("sssi", $name, $email, $comments, $stars);
-        $stmt->execute();
-        echo "Submitted Successfully"
-        $stmt->close();
-        $conn->close();
-    }
+    // // Data connection
+    // $conn =new mysqli('localhost', 'root','','feedback');
+    // if($conn->connect_error){
+    //     die('Connection failed : ' .$conn->connect_error);
+    // }else{
+    //     $stmt = $conn->prepare("insert into feedback(name, email, comments, stars)
+    //     values(?,?,?,?)");
+    //     $stmt->bind_param("sssi", $name, $email, $comments, $stars);
+    //     $stmt->execute();
+    //     echo "Submitted Successfully"
+    //     $stmt->close();
+    //     $conn->close();
+    // }
 ?>
